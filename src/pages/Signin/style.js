f5b209b29081ee76fc2d33d03b5dbcf9fd6afadb
Arthur Diniz <arthurvdinizs@gmail.com>
2019-10-17 21:32:17 -0300

    adição de imagem de background ao login e sign up

--- src/pages/Signin/style.js
+++ src/pages/Signin/style.js
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 import { makeStyles } from '@material-ui/core/styles';
 
+import BackgroundImage from '../../assets/images/background.jpg';
+
 export default {
   Container: styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     height: 100vh;
+    background-image: url(${BackgroundImage});
+    background-size: 25%;
+    background-repeat: repeat;
   `,
   Header: styled.div`
     display: flex;
